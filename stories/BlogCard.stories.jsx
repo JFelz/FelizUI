a3a9b496db00e8b005fa46a390d7b0ad2c53690c
Jovanni Feliz <feliz.jovanni@gmail.com>
2025-05-16 23:16:22 -0500

    Blog Card Completed

---
+++ stories/BlogCard.stories.jsx
@@ -0,0 +1,46 @@
+
+import React from 'react';
+
+import { BlogCard } from '../components/BlogCard';
+
+export default {
+  title: 'Components/Cards/Blog',
+  component: BlogCard,
+  argTypes : {
+    mode: { options: ['primary', 'secondary', 'dark']},
+  }
+}
+
+export const Primary = {
+  args : {
+    name: 'BlogCard',
+    label: 'solid',
+    mode: 'primary',
+    content: {
+      image: '../public/BlogCard/interior.webp',
+      body: 'Curated vibrants colors for your living, make it pop & calm in the same time.',
+    },
+    render: (args) => <BlogCard {...args} />
+  }
+};
+
+export const Secondary = {
+  args : {
+    ...Primary.args,
+    mode: 'secondary',
+    label: 'outline'
+  }
+}
+
+export const DarkMode = {
+  args : {
+    ...Primary.args,
+    mode: 'dark',
+    label: 'solid'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
--- stories/TestimonialCard.stories.jsx
+++ stories/TestimonialCard.stories.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { TestimonialCard } from '../components/TestimonialCard.jsx'
 
 export default {
-  title: 'Components/Cards/TestimonialCard',
+  title: 'Components/Cards/Testimonial',
   component: TestimonialCard,
   argTypes : {
     mode: { options: ['primary', 'secondary', 'dark']},
@@ -23,6 +23,35 @@ export const Primary = {
     content: {
       body: 'I\'ve been searching for high-quality abstract images for my design projects, and I\'m thrilled to have found this platform. The variety and depth of creativity are astounding!'
     },
+    cardOverride: {
+      card: {
+        padding: '24px',
+        width: '340px',
+        height: 'auto',
+        backgroundColor: '#ffffff',
+        borderRadius: '10px',
+        boxShadow: '#0000001f 0px 2px 3px'
+      },
+      nameTitle: {
+        fontSize: '18px',
+        lineHeight: '28px',
+        color: '#171717',
+        width: 'auto',
+        fontWeight: '600',
+      },
+      nameTag: {
+        width: 'auto',
+        fontSize: '14px',
+        lineHeight: '20px',
+        color: '#525252'
+      },
+      bottomCard: {
+        marginTop: '16px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: '#525252'
+      }
+    },
     render: (args) => <TestimonialCard {...args} />
   }
 }
@@ -31,7 +60,40 @@ export const Secondary = {
   args : {
     ...Primary.args,
     mode: 'secondary',
-    label: 'outline'
+    label: 'outline',
+
+    cardOverride: {
+      "card": {
+        "padding": "24px",
+        "width": "340px",
+        "height": "auto",
+        "backgroundColor": "transparent",
+        "borderRadius": "10px",
+        "boxShadow": "#0000001f 0px 2px 3px"
+      },
+
+      "nameTitle": {
+        "fontSize": "18px",
+        "lineHeight": "28px",
+        "color": "#171717",
+        "width": "auto",
+        "fontWeight": "600"
+      },
+
+      "nameTag": {
+        "width": "auto",
+        "fontSize": "14px",
+        "lineHeight": "20px",
+        "color": "#525252"
+      },
+
+      "bottomCard": {
+        "marginTop": "16px",
+        "display": "flex",
+        "flexWrap": "wrap",
+        "color": "#525252"
+      }
+    }
   }
   }
 
@@ -39,7 +101,40 @@ export const DarkMode = {
   args : {
     ...Primary.args,
     mode: 'dark',
-    label: 'solid'
+    label: 'solid',
+
+    cardOverride: {
+      "card": {
+        "padding": "24px",
+        "width": "340px",
+        "height": "auto",
+        "backgroundColor": "#161616",
+        "borderRadius": "10px",
+        "boxShadow": "#0000001f 0px 2px 3px"
+      },
+
+      "nameTitle": {
+        "fontSize": "18px",
+        "lineHeight": "28px",
+        "color": "#acacac",
+        "width": "auto",
+        "fontWeight": "600"
+      },
+
+      "nameTag": {
+        "width": "auto",
+        "fontSize": "14px",
+        "lineHeight": "20px",
+        "color": "#acacac"
+      },
+
+      "bottomCard": {
+        "marginTop": "16px",
+        "display": "flex",
+        "flexWrap": "wrap",
+        "color": "#acacac"
+      }
+    }
   },
   parameters: {
     backgrounds: {
